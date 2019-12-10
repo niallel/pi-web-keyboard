7e901520d0a0ced4f4a9ee4404a6341653c4220d
Niall <niallel@me.com>
2019-12-10 00:41:45 +0000

    Basic web keyboard

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const keyTable = require('../keyboard/keyTable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pi Web Keyboard', keyTable: Object.keys(keyTable) });
 });
 
 module.exports = router;
