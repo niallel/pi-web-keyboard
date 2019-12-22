352d87813e3275ff8c605711970d516c57c79322
Niall <niallel@me.com>
2019-12-22 20:22:57 +0000

    Change api to keyboard

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const apiRouter = require('./api/api');
+const keyboardRouter = require('./api/keyboard');
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+app.use('/keyboard', keyboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
