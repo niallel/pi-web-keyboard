f6d87c04a002f7f82b6189fa2b73c8d5e4831075
Niall <niallel@me.com>
2019-12-23 01:07:00 +0000

    Missing require

--- api/consumer.js
+++ api/consumer.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const sendConsumer = require('../consumer/sendConsumer');
+
 /* API page */
 router.get('/*', function (req, res) {
   const parameters = req.params[0].split('/');
   console.log(`Parameters ${parameters} Number ${parameters.length}`);
 
-  const result = sendKeys(parameters);
+  const result = sendConsumer(parameters);
 
   if (result.command) {
     res.json({
